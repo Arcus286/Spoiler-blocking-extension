7ab0797f9ed5f22031907723b9f88cdd0618e373
Aqua_Blazer <69086207+Arcus286@users.noreply.github.com>
2026-04-13 18:42:34 +0530

    Finished the styling and added the nexessary JS THe styling is done and also the necessary JS operations are done but the main logic of the extension still remains undone

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-const movieText = document.getElementById("keyword-text")
-const submitBtn = document.getElementById("add-btn")
-let movies = []
+const movieText = document.getElementById("keyword-text");
+const submitBtn = document.getElementById("add-btn");
+const keywordList = document.getElementById("keyword-list");
+const deleteBtn = document.getElementById("delete-btn");
+let movies = [];
+
+chrome.storage.local.get(["movie"], (result) => {
+  if (result.movie) {
+    movies = result.movie;
+    renderMovies();
+  }
+});
 
 function renderMovies() {
-    
+  let str = "";
+  for (let i = 0; i < movies.length; i++) {
+    str += `<li id = "movie-list">${movies[i]}</li>`;
+  }
+  keywordList.innerHTML = str;
 }
 
 submitBtn.addEventListener("click", () => {
+  if (movieText.value) {
     movies.push(movieText.value);
-    movieText.innerText = "";
-    renderMovies();
-})
+    movieText.value = "";
+    chrome.storage.local.set({ movie: movies }, () => {
+      renderMovies();
+    });
+  }
+});
+
+deleteBtn.addEventListener("dblclick", () => {
+  chrome.storage.local.clear();
+  movies = [];
+  renderMovies();
+});
